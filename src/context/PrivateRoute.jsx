ec3bf0648be94a2c362a9a0d72eaf43e4fc6a975
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-09-22 13:46:39 +0600

    private route implemented

---
+++ src/context/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import React, { use } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  const location = useLocation()
+  if (loading) {
+    return (<div>
+        <h1>Loading...</h1>
+    </div>)
+  }
+  if (user && user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
+};
+
+export default PrivateRoute;
